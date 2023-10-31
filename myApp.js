c7a62bd9c373ae4f9b3fd36ac3b6d24394571ef6
mano8 <mex.serra@gmail.com>
2023-10-31 03:58:05 +0000

    Set hidePoweredBy to hide X-Powered-By http header

--- myApp.js
+++ myApp.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 const helmet = require('helmet');
-helmet.hidePoweredBy();
+app.use(helmet.hidePoweredBy())
 
 
 
@@ -55,7 +55,7 @@ const api = require('./server.js');
 app.use(express.static('public'));
 app.disable('strict-transport-security');
 app.use('/_api', api);
-app.get("/", function (request, response) {
+app.get("/", function(request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 let port = process.env.PORT || 3000;
